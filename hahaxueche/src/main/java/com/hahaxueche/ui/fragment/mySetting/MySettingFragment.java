47a42846227bc52f460cb72f17739b67211ee2ac
gibxin <243549185@qq.com>
2016-01-24 21:32:50 +0800

    1.添加底部导航栏框架

---
+++ hahaxueche/src/main/java/com/hahaxueche/ui/fragment/mySetting/MySettingFragment.java
@@ -0,0 +1,21 @@
+package com.hahaxueche.ui.fragment.mySetting;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hahaxueche.R;
+
+/**
+ * Created by gibxin on 2016/1/24.
+ */
+public class MySettingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_my_setting, null);
+    }
+}
