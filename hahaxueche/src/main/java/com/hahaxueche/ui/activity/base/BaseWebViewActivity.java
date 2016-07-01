20b8d7ca3ac47c5ff4a1668eb785a46306313f19
gibxin <243549185@qq.com>
2016-07-01 20:33:00 +0800

    banner update

--- hahaxueche/src/main/java/com/hahaxueche/ui/activity/base/BaseWebViewActivity.java
+++ hahaxueche/src/main/java/com/hahaxueche/ui/activity/base/BaseWebViewActivity.java
@@ -45,46 +45,21 @@ public class BaseWebViewActivity extends BaseActivity {
                 return true;
             }
         });
-        WebSettings webSettings = baseWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setUseWideViewPort(true);//关键点
-
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-
-        webSettings.setDisplayZoomControls(false);
-        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
-        webSettings.setAllowFileAccess(true); // 允许访问文件
-        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
-        webSettings.setSupportZoom(true); // 支持缩放
-
-
-        webSettings.setLoadWithOverviewMode(true);
-
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        int mDensity = metrics.densityDpi;
-        if (mDensity == 240) {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
-        } else if (mDensity == 160) {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
-        } else if (mDensity == 120) {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
-        } else if (mDensity == DisplayMetrics.DENSITY_XHIGH) {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
-        } else if (mDensity == DisplayMetrics.DENSITY_TV) {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
-        } else {
-            webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
-        }
-
-
-        /**
-         * 用WebView显示图片，可使用这个参数 设置网页布局类型： 1、LayoutAlgorithm.NARROW_COLUMNS ：
-         * 适应内容大小 2、LayoutAlgorithm.SINGLE_COLUMN:适应屏幕，内容将自动缩放
-         */
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        baseWebView.setInitialScale(80);
+        WebSettings mWebSettings = baseWebView.getSettings();
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setDefaultTextEncodingName("GBK");
+        mWebSettings.setSupportMultipleWindows(true);
+        mWebSettings.setLoadsImagesAutomatically(true);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        mWebSettings.setAppCachePath(appCachePath);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         baseWebView.loadUrl(url);
     }
 
