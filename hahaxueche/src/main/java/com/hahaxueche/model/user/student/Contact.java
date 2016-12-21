a67b8a2e3a36cbeca472839ed3bda3865cc31f42
wangshirui <243549185@qq.com>
2016-12-21 10:57:07 +0800

    read contacts

---
+++ hahaxueche/src/main/java/com/hahaxueche/model/user/student/Contact.java
@@ -0,0 +1,11 @@
+package com.hahaxueche.model.user.student;
+
+/**
+ * 通讯录
+ * Created by wangshirui on 2016/12/21.
+ */
+
+public class Contact {
+    public String name;
+    public String number;
+}
