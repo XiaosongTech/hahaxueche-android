a6c439a0a31b8f289dd3c391334a93071e7c4c84
wangshirui <243549185@qq.com>
2017-01-16 12:08:46 +0800

    more test

--- hahaxueche/src/test/java/com/hahaxueche/ui/activity/login/StartLoginActivityTest.java
+++ hahaxueche/src/test/java/com/hahaxueche/ui/activity/login/StartLoginActivityTest.java
@@ -2,12 +2,14 @@ package com.hahaxueche.ui.activity.login;
 
 import android.content.Intent;
 
+import com.hahaxueche.BuildConfig;
 import com.hahaxueche.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -17,6 +19,7 @@ import static org.robolectric.Shadows.shadowOf;
  * Created by wangshirui on 2017/1/14.
  */
 @RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class StartLoginActivityTest {
     @Test
     public void clickingLogin_shouldStartLoginActivity() {
